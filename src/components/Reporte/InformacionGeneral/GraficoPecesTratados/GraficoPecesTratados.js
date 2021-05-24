2d1e5b7752a84b8a1ebff24dbbb0bc3b1d541f0e
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-05-24 11:01:24 -0400

    agrega grafico peces tratados

--- src/components/Reporte/InformacionGeneral/GraficoPecesTratados/GraficoPecesTratados.js
+++ src/components/Reporte/InformacionGeneral/GraficoPecesTratados/GraficoPecesTratados.js
@@ -1,9 +1,63 @@
 import './GraficoPecesTratados.css'
 
 const GraficoPecesTratados = () => {
+
+  const datos = [
+    {
+      nombre: 'Q1 2020',
+      valor: 0.8
+    },
+    {
+      nombre: 'Q2 2020',
+      valor: 1.2
+    },
+    {
+      nombre: 'Q3 2020',
+      valor: 1.8
+    },
+    {
+      nombre: 'Q4 2020',
+      valor: 1.4
+    },
+    {
+      nombre: 'Q1 2021',
+      valor: 1.7
+    }
+  ]
+
+  const yMax = Math.ceil(datos.reduce((max, v) => Math.max(max, v.valor), 0))
+  const yLineas = [...Array(yMax * 2).fill(0).map((_, i) => i * .5), yMax].reverse()
+
   return (
     <div className="GraficoPecesTratados">
-      GraficoPecesTratados
+      <p className="GraficoPecesTratados__titulo">N° de peces tratados</p>
+      <div className="GraficoPecesTratados__contenedor_grafico">
+        <p className="GraficoPecesTratados__etiqueta_eje_y">
+          Millones
+        </p>
+        <div className="GraficoPecesTratados__contenedor_lineas">
+          {yLineas.map(y => (
+            <div key={`lineay-${y}`} className="GraficoPecesTratados__linea">
+              <p className="GraficoPecesTratados__etiqueta_linea">
+                {y.toLocaleString('de-DE')}
+              </p>
+            </div>
+          ))}
+        </div>
+        {datos.map(d => (
+          <div key={`barra-${d.nombre}`} className="GraficoPecesTratados__contenedor_barra">
+            <div
+              className="GraficoPecesTratados__barra"
+              style={{ '--porcentaje-lleno': `${(d.valor / yMax) * 100}%` }}
+            >
+              {d.valor.toLocaleString('de-DE')}
+            </div>
+            <div className="GraficoPecesTratados__etiqueta_barra">
+              {d.nombre}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
