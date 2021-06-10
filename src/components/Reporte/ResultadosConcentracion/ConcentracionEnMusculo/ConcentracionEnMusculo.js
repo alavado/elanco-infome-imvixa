a3b5ae122c0336ac3f50bf452fe2dbb61a8c2d2b
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-06-10 17:23:53 -0400

    agrega concentracion en musculo

--- src/components/Reporte/ResultadosConcentracion/ConcentracionEnMusculo/ConcentracionEnMusculo.js
+++ src/components/Reporte/ResultadosConcentracion/ConcentracionEnMusculo/ConcentracionEnMusculo.js
@@ -1,11 +1,95 @@
 import './ConcentracionEnMusculo.css'
 
 const ConcentracionEnMusculo = () => {
+
+  const datos = [
+    {
+      nombre: 'Q1 2020',
+      promedio: 22,
+      iqr: 3,
+      max: 29,
+      min: 5
+    },
+    {
+      nombre: 'Q1 2021',
+      promedio: 22,
+      iqr: 3,
+      max: 35,
+      min: 5
+    },
+    {
+      nombre: 'Q2 2021',
+      promedio: 20,
+      iqr: 4,
+      max: 39,
+      min: 0
+    },
+    {
+      nombre: 'Q3 2021',
+      promedio: 21,
+      iqr: 4,
+      max: 39,
+      min: 5
+    },
+    {
+      nombre: 'Q4 2021',
+      promedio: 22,
+      iqr: 4,
+      max: 35,
+      min: 8
+    }
+  ]
+
+  const vMax = Math.ceil(datos.reduce((max, v) => Math.max(max, v.promedio), 0))
+  const vMin = Math.floor(datos.reduce((min, v) => Math.min(min, v.promedio), Infinity))
+  const tick = Math.pow(10, Math.floor(Math.log10(vMin)))
+  const yMax = Math.max(60, 10 * Math.ceil(vMax / tick))
+  const yMin = Math.min(0, 10 * Math.floor(vMin / tick))
+  const yLineas = [...Array(Math.round(1 + (yMax - yMin) / tick)).fill(0).map((_, i) => yMin + tick * i)].reverse()
+
   return (
     <div className="ConcentracionEnMusculo">
       <p className="ConcentracionEnMusculo__titulo">
-        Concentración (ppb) en músculo post tratamiento
+        Concentración (ppb) en músculo 
+        post tratamiento
       </p>
+      <div className="ConcentracionEnMusculo__contenedor_grafico">
+        <p className="ConcentracionEnMusculo__etiqueta_eje_y">
+          Miles
+        </p>
+        <div className="ConcentracionEnMusculo__contenedor_lineas">
+          {yLineas.map(y => (
+            <div key={`lineay-${y}`} className="ConcentracionEnMusculo__linea">
+              <p className="ConcentracionEnMusculo__etiqueta_linea">
+                {y.toLocaleString('de-DE')}
+              </p>
+            </div>
+          ))}
+        </div>
+        {datos.map(d => (
+          <div key={`caja-cc-${d.nombre}`} className="ConcentracionEnMusculo__contenedor_caja">
+            <div
+              className="ConcentracionEnMusculo__bigote"
+              style={{
+                '--porcentaje-top': `${((yMax - d.max) / (yMax - yMin)) * 100}%`,
+                'height': `${((d.max - d.min) / (yMax - yMin)) * 100}%`
+              }}
+            />
+            <div
+              className="ConcentracionEnMusculo__caja"
+              style={{
+                '--porcentaje-bottom': `${((d.promedio - d.iqr - yMin) / (yMax - yMin)) * 100}%`,
+                '--porcentaje-top': `${((yMax - d.iqr - d.promedio) / (yMax - yMin)) * 100}%`
+              }}
+            >
+              {d.promedio.toFixed(0).toLocaleString('de-DE')}M
+            </div>
+            <div className="ConcentracionEnMusculo__etiqueta_caja">
+              {d.nombre.split(' ').map((n, i) => <div key={`${d.nombre}-${i}`}>{n}</div>)}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
