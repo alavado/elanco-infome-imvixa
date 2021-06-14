9c891e5e5916e0850c88d4dcce937ef26dfc2b33
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-06-14 14:33:36 -0400

    impresion casi lista

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
+const { ipcMain } = require('electron')
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const fs = require('fs')
 
 const path = require('path');
 const isDev = require('electron-is-dev');
@@ -8,8 +10,22 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    }
+  });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  isDev || mainWindow.removeMenu()
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.maximize()
+    mainWindow.show()    
+  })
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
@@ -31,3 +47,21 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+ipcMain.on('imprimir', async (event, state) => {
+  try {
+    const data = await mainWindow.webContents.printToPDF({
+      printBackground: true,
+      marginsType: 1,
+      pageSize: {
+        width: 352,
+        height: 638.8096
+      }
+    })
+    fs.writeFileSync(path.join(app.getPath('desktop'), 'Informe Seguimiento Imvixa.pdf'), data)
+    await electron.shell.openPath(path.join(app.getPath('desktop'), 'Informe Seguimiento Imvixa.pdf'))
+  }
+  catch(err) {
+    console.log('err', err)
+  }
+})
--- src/components/Reporte/Reporte.js
+++ src/components/Reporte/Reporte.js
@@ -8,19 +8,30 @@ import InformacionGeneral from './InformacionGeneral/InformacionGeneral'
 import ResultadosEficacia from './ResultadosEficacia'
 import ResultadosConcentracion from './ResultadosConcentracion'
 import Sandalias from './Sandalias'
+const { ipcRenderer } = window.require('electron')
 
 const Reporte = () => {
+
+  const imprimirPDF = () => {
+    ipcRenderer.send('imprimir')
+  }
+
   return (
     <div className="Reporte">
-      <Link className="Reporte__volver" to="/">❮ Parámetros</Link>
-      <img src={logoImvixa} className="Reporte__logo_imvixa" alt="Logo Imvixa" />
-      <img src={logoElanco} className="Reporte__logo_elanco" alt="Logo Elanco" />
-      <img src={salmon} className="Reporte__salmon" alt="Salmón acuático" />
-      <DatosEmpresa />
-      <InformacionGeneral />
-      <ResultadosConcentracion />
-      <ResultadosEficacia />
-      <Sandalias />
+      <div className="Reporte__herramientas">
+        <Link className="Reporte__volver" to="/">❮ Parámetros</Link>
+        <button onClick={imprimirPDF}>Exportar</button>
+      </div>
+      <div className="Reporte__contenedor">
+        <img src={logoImvixa} className="Reporte__logo_imvixa" alt="Logo Imvixa" />
+        <img src={logoElanco} className="Reporte__logo_elanco" alt="Logo Elanco" />
+        <img src={salmon} className="Reporte__salmon" alt="Salmón acuático" />
+        <DatosEmpresa />
+        <InformacionGeneral />
+        <ResultadosConcentracion />
+        <ResultadosEficacia />
+        <Sandalias />
+      </div>
     </div>
   )
 }
