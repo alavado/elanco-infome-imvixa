012344c1564368be4de8d2d646d0150b377bcfd0
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-09-22 16:06:58 -0300

    corrige importacion de datos de graficos en main thread

--- public/electron.js
+++ public/electron.js
@@ -3,7 +3,36 @@ const { ipcMain, Menu, MenuItem, ipcRenderer } = require('electron')
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const fs = require('fs')
-const { graficos } = require('../src/helpers/graficos') 
+const graficos = [
+  {
+    id: 'GRAFICO_PECES_TRATADOS',
+    titulo: 'Peces tratados en el periodo'
+  },
+  {
+    id: 'GRAFICO_PESO_PROMEDIO',
+    titulo: 'Peso promedio'
+  },
+  {
+    id: 'GRAFICO_CUMPLIMIENTO',
+    titulo: 'Porcentaje incorporación en alimento'
+  },
+  {
+    id: 'GRAFICO_CONCENTRACION_MUSCULO',
+    titulo: 'Concentración en músculo post tratamiento'
+  },
+  {
+    id: 'GRAFICO_COMPARACION_CONCENTRACION',
+    titulo: 'Concentración en músculo por piscicultura'
+  },
+  {
+    id: 'GRAFICO_EFICACIA',
+    titulo: 'Eficacia en agua de mar'
+  },
+  {
+    id: 'GRAFICO_MACROZONAS',
+    titulo: 'Mapa protección histórica en macrozonas'
+  }
+]
 
 const { autoUpdater } = require('electron-updater')
 const path = require('path');
