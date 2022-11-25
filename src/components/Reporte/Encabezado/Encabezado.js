207f38d8511c79af5691c727ded51a5bfb13b3a2
cinai <ceinaipil@gmail.com>
2022-11-25 19:04:09 -0300

    cambiar logos

--- src/components/Reporte/Encabezado/Encabezado.js
+++ src/components/Reporte/Encabezado/Encabezado.js
@@ -6,44 +6,39 @@ import tituloReporte1 from '../../../assets/images/Reporte Concentración en Ali
 import tituloReporte2 from '../../../assets/images/Reporte de Concentración en Músculo Piel.svg'
 import tituloReporte3 from '../../../assets/images/Reporte Seguimiento por Centro de Mar.svg'
 import tituloReporte4 from '../../../assets/images/Reporte de Seguimiento.svg'
+import titleReporte1 from '../../../assets/images/In-Feed Concentration Report_Mesa de trabajo 1.svg'
+import titleReporte2 from '../../../assets/images/Muscle skin concentration report_Mesa de trabajo 1.svg'
+import titleReporte3 from '../../../assets/images/Marine Site Follow-up Report_Mesa de trabajo 1.svg'
+import titleReporte4 from '../../../assets/images/Follow-Up Report IMVIXA_Mesa de trabajo 1.svg'
 
 const Encabezado = ({ reporteID, reporteNombre, language }) => {
 	let titulo, imgClassName, title;
 		switch (reporteID) {
 			case 1:
-				titulo = tituloReporte1
+				titulo = language === 'es' ? tituloReporte1 : titleReporte1
 				imgClassName = "Reporte__logo_imagen_alimento"
 				title = 'IN-FEED CONCENTRATION REPORT'
 				break;
 			case 2:
-				titulo = tituloReporte2
+				titulo =  language === 'es' ? tituloReporte2 : titleReporte2
 				imgClassName = "Reporte__logo_imagen_musculo"
 				title = 'MUSCLE/SKIN CONCENTRATION REPORT'
 				break;
 			case 3:
-				titulo = tituloReporte3
+				titulo = language === 'es' ? tituloReporte3 : titleReporte3
 				imgClassName = "Reporte__logo_imagen_alimento"
 				title = 'MARINE SITE FOLLOW-UP REPORT'
 				break;
 			default:
-				titulo = tituloReporte4
+				titulo = language === 'es' ? tituloReporte4 : titleReporte4
 				imgClassName = "Reporte__logo_imagen_seguimiento"
 				title = 'FOLLOW-UP REPORT IMVIXA'
 				break;
 		}
-	if (language === 'es') {
-		return (
-			<div className='Encabezado'>
-				<img src={logoImvixa} className="Reporte__logo_imvixa" alt="Logo Imvixa" />
-				<img src={titulo} className={imgClassName} alt={reporteNombre} style={{paddingLeft:'0.25rem'}}/>
-				<img src={logoElanco} className="Reporte__logo_elanco" alt="Logo Elanco" />
-			</div>
-		)
-	}
 	return (
 		<div className='Encabezado'>
 			<img src={logoImvixa} className="Reporte__logo_imvixa" alt="Logo Imvixa" />
-			<h1 className={imgClassName} alt={title} style={{paddingLeft:'0rem'}}>{title} </h1>
+			<img src={titulo} className={imgClassName} alt={reporteNombre} style={{paddingLeft:'0.25rem'}}/>
 			<img src={logoElanco} className="Reporte__logo_elanco" alt="Logo Elanco" />
 		</div>
 	)
